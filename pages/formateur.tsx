83621096704948f80978b99bfc4bf065b4997a14
Marine <101167755+Kaa-b@users.noreply.github.com>
2023-03-08 10:52:09 +0000

    starting formateur page

--- pages/formateur.tsx
+++ pages/formateur.tsx
@@ -1,20 +1,39 @@
 import Head from 'next/head';
+import Image from 'next/image';
 
 import Navbar from 'components/nav/Navbar';
 import Footer from 'components/footer/Footer';
 
+import styles from '../styles/formateur.module.css';
+
 export default function Formateur() {
   return (
-    <>
+    <div className={styles.container}>
       <Head>
         <title>Formateur</title>
         <meta name="description" content="Page Formateur" />
       </Head>
       <Navbar />
       <main>
-        Page Formateur
+        <section className={styles.header}>
+          <div className={styles.headerPicture}>
+              <Image
+                src="/static/heroImg.jpg"
+                alt="Hero image girl fighting"
+                width={400}
+                height={550}
+              />
+          </div>
+          <div className={styles.headerPresentation}>
+            <p>Présentation un peu plus détaillée de Amar</p>
+          </div>
+        </section>
+        <section className={styles.links}> 
+          <div className={styles.linkContent}></div>
+          <div className={styles.linkPictures}></div>
+        </section>
       </main>
       <Footer/>
-    </>
+    </div>
   );
 }
